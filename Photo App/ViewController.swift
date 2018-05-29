44f329c1ba6d19e7c305c40b31341cf97674c563
Mohamed Hamed <mohamedhamed@Mohameds-Mac.local>
2018-05-29 03:06:45 +0200

    initial commit

---
+++ Photo App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Photo App
+//
+//  Created by Mohamed Hamed on 5/29/18.
+//  Copyright © 2018 Mohamed Hamed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
